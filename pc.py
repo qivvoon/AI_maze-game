85dd97667e62a0d083b787147ad20ec2a654ade4
qivvoon <gangjiwon70@gmail.com>
2023-04-19 22:47:51 +0900

    pc.py

---
+++ pc.py
@@ -0,0 +1,18 @@
+import pygame
+from pygame.locals import *
+import pgu
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((600, 600))
+pygame.display.set_caption('example')
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+pygame.quit()
+sys.exit()
